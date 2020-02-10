9b7d0e3da0ceeb073810752fa6b55a908e2ed4e5
uk <235qwerty@naver.com>
2020-02-10 16:27:01 +0900

    append Hi-origin append Hi-ET

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
 		System.out.println("Hello");
 		System.out.println("Hello-origin");
 		System.out.println("Hi-master");
+		System.out.println("Hi-origin");
+		System.out.println("Hi-ET");
 	}
 }
